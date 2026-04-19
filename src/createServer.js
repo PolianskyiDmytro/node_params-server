1e9969d64480f51f6dbcef506be0498818e4b33f
Polianskyi Dmytro <dimdemon1@gmail.com>
2026-04-19 11:41:34 +0300

    add solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,27 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
   /* Write your code here */
   // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const url = new URL(req.url, `http://localhost:5701`);
+
+    const normalizedPathname = url.pathname.replace(/\/+/g, '/');
+    const parts = normalizedPathname.slice(1).split('/');
+    const query = Object.fromEntries(url.searchParams);
+    const result = {
+      parts,
+      query,
+    };
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));
+  });
 }
 
 module.exports = {
